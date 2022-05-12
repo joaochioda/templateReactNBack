396704a94061a3eabc3711fa6083dce8934e4a34
joaochioda <joao.chioda@dextra-sw.com>
2022-05-12 19:35:03 -0300

    service

---
+++ frontend/template-app/src/service/httpService.ts
@@ -0,0 +1,39 @@
+/* eslint-disable prettier/prettier */
+import axios from "axios";
+const baseUrl = "http://localhost:3000/api/";
+// Add a request interceptor
+axios.interceptors.request.use(
+  function (config) {
+    config.baseURL = baseUrl;
+
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  (response) => {
+    if (response.status === 401) {
+      return "Unauthorized";
+    }
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.data) {
+      if (error.response.status === 401) {
+        return "Unauthorized";
+      }
+    }
+  }
+);
+
+export default {
+  get: axios.get,
+  post: axios.post,
+  put: axios.put,
+  delete: axios.delete,
+  patch: axios.patch,
+};
