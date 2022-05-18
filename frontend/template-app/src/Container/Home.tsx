72bce5e8fb737d2a041f7ccda5ede819568696a0
joaochioda <joao.chioda@dextra-sw.com>
2022-05-18 18:25:14 -0300

    base path

--- frontend/template-app/src/Container/Home.tsx
+++ frontend/template-app/src/Container/Home.tsx
@@ -1,7 +1,6 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import { UserContext } from "../Contexts/userContext";
-import { UserContextType } from "../Contexts/userContext";
+import { UserContextType, UserContext } from "src/Contexts/userContext";
 import { HomeContainer } from "./Style";
 
 export function Home() {
